8dae33f591f0292c0077b36d5bdcc01db0615782
SowmyaAavula <83690525+SowmyaAavula@users.noreply.github.com>
2021-05-05 13:55:28 +0530

    Add files via upload

--- Department.java
+++ Department.java
@@ -5,13 +5,19 @@ package com.dept;
 		private int departmentNumber; //same as deptno column
 		private String departmentName; // same as dname column
 		private String departmentLocation; //same as loc column
-		
+		private int departmentId;
 		//setter and getter to set and fetch information 
 		public int getDepartmentNumber() {
 			return departmentNumber;
 		}
 		public void setDepartmentNumber(int departmentNumber) {
 			this.departmentNumber = departmentNumber;
+		}
+		public int getDepartmentId() {
+			return departmentId;
+		}
+		public void setDepartmentId(int departmentId) {
+			this.departmentId = departmentId;
 		}
 		
 		public String getDepartmentName() {
